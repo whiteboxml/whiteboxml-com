583effd44a6f63a23527c858e46f82bf1e4277c7
Pedro <p.munozb2103@gmail.com>
2019-10-17 14:16:35 +0200

    Initial commit

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.sitemaps.views import sitemap
+from django.urls import path
+
+from . import views
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
+
+urlpatterns = [
+    path('', views.index, name='landing'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
+]
